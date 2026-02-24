5908dd65fc8c282e180bc22e94aa546145869331
Zeus Cach <83797099+zeusCach@users.noreply.github.com>
2026-02-24 16:26:26 -0500

    Añadiendo primer desafio de la actividad AA 1.3 Module JS modulosNode

--- ModulosNode/Index.js
+++ ModulosNode/Index.js
@@ -1,18 +1,17 @@
-const { error } = require('console');
 const fs = require('fs');
 
-// fs.writeFile('archivo.txt', 'Hola desde NodeJS', (err) => {
-//     if(err) throw new err
-//     console.log('El archivo ha sido creado con exito');
-// })
+//Creamos el archivo.txt con el text Hola desde node
+fs.writeFile('archivo.txt', 'Hola desde NodeJS', (err) => {
+    if(err) throw new err
+     console.log('El archivo ha sido creado con exito');
+})
 
 
 //path a la ruta del archivo
 const path = 'archivo.txt';
 
 //funcion de readFile que recibe un callback para imprimir lo que contiene nuestro archivo segun el path
-
 fs.readFile(path, 'utf-8', (err, data) => {
-        if(err) throw new err
-        console.log(data);
+    if(err) throw new err
+    console.log(data);
 })
